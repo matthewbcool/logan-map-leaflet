ae4337424aef2b545af6b1f748b64e3cda72df99
Matt Cool <matthewbcool@gmail.com>
2018-09-17 13:37:49 -0500

    experiment with removeLayer function

--- src/Components/LoganMap.jsx
+++ src/Components/LoganMap.jsx
@@ -5,26 +5,40 @@ import L from 'leaflet';
 
 class LoganMap extends React.Component {
   componentDidMount() {
+    let markerData = this.props.markerData;
+    
+    
     // create map
     this.map = L.map('map', {
       center: [41.9308, -87.7099],
       zoom: 15,
       layers: [
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }),
+  
       ]
     });
     //TO DO: use layerGroup class in leaflet to add this as the default layer...
-    let markerData = this.props.markerData;
+    const defaultLayer = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    })
+    this.map.addLayer(defaultLayer)
+    
+    let defaultMarkerArray = [];
+    let layerGroup = L.layerGroup().addTo(this.map)
+    
     markerData.forEach(element => {
-      this.marker = L.marker(element.marker).addTo(this.map).bindPopup(element.placeName)
-    }
-    )
+        let currentMarker = L.marker(element.marker).bindPopup(element.placeName).addTo(layerGroup)
+        defaultMarkerArray.push(currentMarker)
+        })   
+    const removeDefaultLayer = () => {
+      this.map.removeLayer(layerGroup) 
+    }      
   }
-
-  componentDidUpdate() {
-   this.props.filteredMarkerData.forEach(element => console.log(element))
+  
+  componentDidUpdate(removeDefaultLayer) {
+    removeDefaultLayer()
+   //layerGroup.removeLayer(layerGroup.markerData)
+   //this.props.filteredMarkerData.forEach(element => console.log(element))
+   
   }
   
   render() {
